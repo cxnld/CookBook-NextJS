1346a71e735ac6ccea2e2e3789e0cf37f089e794
Cathan Hui <cathan14@gmail.com>
2021-07-19 20:31:08 -0700

    First Update

--- components/RecipeGrid/index.js
+++ components/RecipeGrid/index.js
@@ -1,22 +1,11 @@
-import Link from 'next/link'
-
 import GridElement from './GridElement'
-import Button from 'react-bootstrap/Button'
 
 const RecipeGrid = ({ recipes }) => {
     return (
-        <div>
-            <div className="recipe-header">
-                <h1>My Recipes</h1>
-                <Link href="/recipes/new" passHref>
-                    <Button variant="primary">New Recipe</Button>
-                </Link>
-            </div>
-            <div className="grid">
-                {recipes.map(recipe => (
-                    <GridElement key={recipe._id} recipe={recipe}></GridElement>
-                ))}
-            </div>
+        <div className="grid">
+            {recipes.map(recipe => (
+                <GridElement key={recipe._id} recipe={recipe}></GridElement>
+            ))}
         </div>
     )
 }
--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 import { getAllRecipes } from '../../utils/http-requests'
 
 import RecipeGrid from '../../components/RecipeGrid'
+import Button from 'react-bootstrap/Button'
 import Spinner from 'react-bootstrap/Spinner'
 
 const Recipes = () => {
@@ -30,7 +32,16 @@ const Recipes = () => {
                 <Head>
                     <title>CookBook | Recipes</title>
                 </Head>
-                
+
+                <div className="recipe-header">
+                    <h1>My Recipes</h1>
+                    <Link href="/recipes/new" passHref>
+                        <Button variant="primary">New Recipe</Button>
+                    </Link>
+                </div>
+
+                { data.length == 0 && <h5>You have no recipes.</h5> }
+
                 <RecipeGrid recipes={data}/>
             </>
         )
